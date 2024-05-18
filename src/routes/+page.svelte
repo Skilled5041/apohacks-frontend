022c89cefdfb1bb37e2d5b59cd1ccb12c077b59b
Skilled5041 <skilled5041@gmail.com>
2024-05-18 10:59:18 -0400

    5th commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-    import VideoStream from "$lib/components/VideoStream.svelte";
-    let media: Blob[] = [];
+	import VideoStream from "$lib/components/VideoStream.svelte";
+
+	let media: Blob[] = [];
 	let mediaRecorder: MediaRecorder | null = null;
-    let recording = false;
+	let recording = false;
 
 	const sendFile = async (file: File) => {
 		const formData = new FormData();
 		formData.append("file", file);
 		const response = await fetch("http://localhost:3000/upload", {
 			method: "POST",
-			body: formData,
+			body: formData
 		});
 		const data = await response.json();
 		console.log(data);
 	};
 
-    let volume: number = 0;
+	let volume: number = 0;
 
 	onMount(async () => {
 		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -28,54 +29,35 @@
 			if (audio?.src) {
 				audio.src = window.URL.createObjectURL(blob);
 			}
-            sendFile(new File([blob], "audio.ogg"));
-        };
-        const audioContext = new AudioContext();
-        const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
-        const analyserNode = audioContext.createAnalyser();
-        mediaStreamAudioSourceNode.connect(analyserNode);
+			sendFile(new File([blob], "audio.ogg"));
+		};
+		const audioContext = new AudioContext();
+		const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
+		const analyserNode = audioContext.createAnalyser();
+		mediaStreamAudioSourceNode.connect(analyserNode);
 
-        const pcmData = new Float32Array(analyserNode.fftSize);
-        const onFrame = () => {
-            analyserNode.getFloatTimeDomainData(pcmData);
-            let sumSquares = 0.0;
-            for (const amplitude of pcmData) { sumSquares += amplitude*amplitude; }
-            volume = Math.sqrt(sumSquares / pcmData.length);
-            window.requestAnimationFrame(onFrame);
-        };
-        window.requestAnimationFrame(onFrame);
-    });
+		const pcmData = new Float32Array(analyserNode.fftSize);
+		const onFrame = () => {
+			analyserNode.getFloatTimeDomainData(pcmData);
+			let sumSquares = 0.0;
+			for (const amplitude of pcmData) {
+				sumSquares += amplitude * amplitude;
+			}
+			volume = Math.sqrt(sumSquares / pcmData.length);
+			window.requestAnimationFrame(onFrame);
+		};
+		window.requestAnimationFrame(onFrame);
+	});
 	const startRecording = () => {
 		media = [];
 		mediaRecorder?.start();
-        recording = true;
+		recording = true;
 	};
 	const stopRecording = () => {
 		mediaRecorder?.stop();
-        recording = false;
+		recording = false;
 	};
 </script>
-
-
-<div class="p-12 flex flex-col items-center">
-	<h1 class="minecraft">test</h1>
-	<VideoStream/>
-	<div class="pt-12" />
-	{#if recording}
-		<button class="stack w-80 bg-black text-white h-24 rounded-3xl inline-flex justify-center items-center outline outline-2 outline-white" style="--stacks: 3;" on:click={startRecording}>
-			<span class="py=8" style="--index: 0;">Stop</span>
-			<span class="py-8" style="--index: 1;">Stop</span>
-			<span class="py-8" style="--index: 2;">Stop</span>
-		</button>
-	{:else}
-		<button class="stack w-80 bg-black text-white h-24 rounded-3xl inline-flex justify-center items-center outline outline-2 outline-white" style="--stacks: 3;" on:click={startRecording}>
-			<span class="py=8" style="--index: 0;">Record</span>
-			<span class="py-8" style="--index: 1;">Record</span>
-			<span class="py-8" style="--index: 2;">Record</span>
-		</button>
-	{/if}
-	<meter id="volumeMeter" high={0.25} max={1} value={volume}></meter>
-</div>
 
 <div class="p-12">
 	<br>
@@ -96,18 +78,24 @@
 	<h1 class="minecraft">test</h1>
 	<div class="pt-12" />
 	{#if recording}
-		<button class="hover-effect stack w-80 bg-black text-white h-24 rounded-3xl inline-flex justify-center items-center outline outline-2 outline-white" style="--stacks: 3;" on:click={stopRecording}>
+		<button
+			class="hover-effect stack w-80 bg-black text-white h-24 rounded-3xl inline-flex justify-center items-center outline outline-2 outline-white"
+			style="--stacks: 3;" on:click={stopRecording}>
 			<span class="py=8" style="--index: 0;">Stop</span>
 			<span class="py-8" style="--index: 1;">Stop</span>
 			<span class="py-8" style="--index: 2;">Stop</span>
 		</button>
 	{:else}
-		<button class="hover-effect stack w-80 bg-black text-white h-24 rounded-3xl inline-flex justify-center items-center outline outline-2 outline-white" style="--stacks: 3;" on:click={startRecording}>
+		<button
+			class="hover-effect stack w-80 bg-black text-white h-24 rounded-3xl inline-flex justify-center items-center outline outline-2 outline-white"
+			style="--stacks: 3;" on:click={startRecording}>
 			<span class="py=8" style="--index: 0;">Record</span>
 			<span class="py-8" style="--index: 1;">Record</span>
 			<span class="py-8" style="--index: 2;">Record</span>
 		</button>
 	{/if}
+	<meter class="mt-12" id="volumeMeter" high={0.25} max={1} value={volume}></meter>
+	<VideoStream />
 </div>
 
 <style>
@@ -130,12 +118,12 @@
         --font-name: Righteous;
     }
 
-	.hover-effect {
-		transition: all 0.3s;
+    .hover-effect {
+        transition: all 0.3s;
     }
 
-	.hover-effect:hover {
-		transform: scale(1.05) rotate(2deg) perspective(100px) rotateX(2deg);
+    .hover-effect:hover {
+        transform: scale(1.05) rotate(2deg) perspective(100px) rotateX(2deg);
     }
 
     @font-face {
@@ -143,9 +131,9 @@
         src: url('/fonts/minecraft_font.ttf') format('truetype');
     }
 
-	.minecraft {
-		font-family: "Minecraft", sans-serif;
-	}
+    .minecraft {
+        font-family: "Minecraft", sans-serif;
+    }
 
     .c-rainbow {
         counter-reset: rainbow;
